62f09b9d33b2d0ed2d083750efa4489133cc3691
julie kim <julie.kim.dev@gmail.com>
2024-02-27 23:46:26 +0900

    🎨 /stores path index.js 등록

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import AuthRouter from "../routes/auth.router.js";
 import UsersRouter from "../routes/users.router.js";
+import StoresRouter from "../routes/stores.router.js";
 
 const router = express.Router();
 
 router.use("/auth", AuthRouter);
 router.use("/user", UsersRouter);
+router.use("/stores", StoresRouter);
 
 export default router;
