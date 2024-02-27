85ce8500d687b2e134cf3455054e1b7cd309fa37
julie kim <julie.kim.dev@gmail.com>
2024-02-27 21:06:46 +0900

    🚧 throw 변경, status 추가

--- src/services/stores.service.js
+++ src/services/stores.service.js
@@ -1,3 +1,5 @@
+import { ValidationError, NotFoundError } from "../utils/common.error.js";
+
 export class StoresService {
   constructor(storesRepository) {
     this.storesRepository = storesRepository;
@@ -16,56 +18,43 @@ export class StoresService {
   };
 
   // 음식점 생성
-  createStore = async ({ name, address, category }) => {
+  createStore = async ({ name, address, category, status }) => {
     await storesRepository.createStore({
       name,
       address,
       category,
+      status: "opened",
     });
   };
 
   // 음식점 수정
-  updateStore = async (storeId, data, byUser) => {
+  updateStore = async (storeId, data, userId) => {
     const store = await storesRepository.findOneStore(storeId);
 
     // res는 컨트롤러에서 사용하는 것이기 때문에 throw로 변경
     if (!store) {
-      throw {
-        code: 401,
-        message: "존재하지 않는 음식점입니다.",
-      };
+      throw new NotFoundError("존재하지 않는 음식점입니다.");
     }
 
-    const { name, address, category } = data;
+    const { name, address, category, status } = data;
     await storesRepository.updateStore(storeId, {
       name,
       address,
       category,
+      status,
     });
   };
 
   // 음식점 삭제
-  deleteStore = async (storeId, byUser) => {
+  deleteStore = async (storeId, userId) => {
     const store = await storesRepository.findOneStore(storeId);
 
-    if (!storeId) {
-      throw {
-        code: 400,
-        message: "storeId는 필수값입니다.",
-      };
-    }
     if (!store) {
-      throw {
-        code: 400,
-        message: "음식점 조회에 실패했습니다.",
-      };
+      throw new NotFoundError("음식점 조회에 실패했습니다.");
     }
 
-    if (store.userId !== byUser.userId) {
-      throw {
-        code: 400,
-        message: "올바르지 않은 요청입니다.",
-      };
+    if (store.userId !== userId) {
+      throw new NotFoundError("올바르지 않은 요청입니다.");
     }
 
     await storesRepository.deleteStore(storeId);
