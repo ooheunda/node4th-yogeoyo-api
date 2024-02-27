6c5484123bf7f05f0599e599aba0a1328d3a5dbe
julie kim <julie.kim.dev@gmail.com>
2024-02-27 23:45:00 +0900

    🚧 라우터 의존성 주입, /stores path index.js 등록

--- src/routes/stores.router.js
+++ src/routes/stores.router.js
@@ -1,23 +1,28 @@
 import express from "express";
+import { prisma } from "../utils/prisma.client.js";
 import { StoresController } from "../controllers/stores.controller.js";
+import { StoresService } from "../services/stores.service.js";
+import { StoresRepository } from "../repositories/stores.repository.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
-const storesController = new StoresController();
+const storesController = new StoresController(storesService);
+const storesService = new StoresService(storesRepository);
+const storesRepository = new StoresRepository(prisma);
 
 // 음식점 목록 조회
-router.get("/stores", storesController.findAllStores);
+router.get("/", storesController.findAllStores);
 
 // 음식점 상세 조회
-router.get("/stores/:storeId", storesController.findOneStore);
+router.get("/:storeId", storesController.findOneStore);
 
 // 음식점 생성
-router.post("/stores", authMiddleware, storesController.createStore);
+router.post("/", authMiddleware, storesController.createStore);
 
 // 음식점 수정
-router.patch("/stores/:storeId", authMiddleware, storesController.updateStore);
+router.patch("/:storeId", authMiddleware, storesController.updateStore);
 
 // 음식점 삭제
-router.delete("/stores/:storeId", authMiddleware, storesController.deleteStore);
+router.delete("/:storeId", authMiddleware, storesController.deleteStore);
 
 export default router;
