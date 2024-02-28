2c072d7b4400692f0a09bd15129229a073e339a8
ooheunda <ooheunda@gmail.com>
2024-02-28 16:16:11 +0900

    :technologist: 리다이렉트 삭제(튜터님 질답 반영)

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -7,7 +7,10 @@ export default async (req, res, next) => {
     const { accessToken, refreshToken } = req.cookies;
     if (!accessToken && !refreshToken)
       throw new UnauthorizedError("로그인이 필요합니다.");
-    if (!accessToken && refreshToken) return res.redirect("/auth/tokens");
+    if (!accessToken && refreshToken)
+      return res
+        .status(400)
+        .json({ message: "토큰 재발급을 한 번 받아보실래요?" });
 
     const decodedToken = verifyAccessToken(accessToken);
     const userId = decodedToken.userId;
@@ -31,7 +34,7 @@ export default async (req, res, next) => {
 
     switch (err.name) {
       case "TokenExpiredError":
-        return res.redirect("/auth/tokens");
+        return res.status(401).json({ message: "토큰이 만료되었습니다." });
 
       case "JsonWebTokenError":
         return res.status(401).json({ message: "토큰이 조작되었습니다." });
